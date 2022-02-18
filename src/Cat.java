b3221aed4bd0f2a9c8000fbab739c854c538335b
Zinedine Boumghar <zboumghar@mines.edu>
2022-02-17 18:33:16 -0700

    Changed the mouth color to green by Zinedine Boumghar

--- src/Cat.java
+++ src/Cat.java
@@ -39,7 +39,7 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.green);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
