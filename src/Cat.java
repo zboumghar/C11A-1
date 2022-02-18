690f2493bc436f81d3bc5b9a1f6550107bd73387
Zinedine Boumghar <zboumghar@mines.edu>
2022-02-17 18:27:53 -0700

    Changed the noise the cat makes to a proper message by Zinedine Boumghar

--- src/Cat.java
+++ src/Cat.java
@@ -46,6 +46,6 @@ public class Cat {
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("SENTINENT CAT", catX, catY+HEAD_DIMENSION+10);	
 	}
 }
